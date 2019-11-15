2627310200c613d493456c3e6f9e4db6e8219072
wangliuheng <18236169404@163.com>
2019-11-15 14:48:29 +0800

    1111

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -51,8 +51,6 @@ export default {
     }
   },
   data() {
-    // To fix https://github.com/PanJiaChen/vue-admin-template/issues/237
-    // TODO: refactor with render function
     this.onlyOneChild = null
     return {}
   },
@@ -62,32 +60,29 @@ export default {
         if (item.hidden) {
           return false
         } else {
-          // Temp set(will be used if only has one showing child)
+          // 临时设置(如果只有一个显示的子节点，则使用临时设置)
           this.onlyOneChild = item
           return true
-        }
-      })
-
-      // When there is only one child router, the child router is displayed by default
+        };
+      });
+      // 当只有一个子路由器时，默认显示子路由器
       if (showingChildren.length === 1) {
         return true
-      }
-
-      // Show parent if there are no child router to display
+      };
+      // 没有要显示的子路由器，则显示父路由器
       if (showingChildren.length === 0) {
         this.onlyOneChild = { ... parent, path: '', noShowingChildren: true }
         return true
-      }
-
+      };
       return false
     },
     resolvePath(routePath) {
       if (isExternal(routePath)) {
         return routePath
-      }
+      };
       if (isExternal(this.basePath)) {
         return this.basePath
-      }
+      };
       return path.resolve(this.basePath, routePath)
     }
   }
